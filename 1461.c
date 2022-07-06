ec61c2cb3a392cc6b169dd482fbfb834c27d26d8
minsung060607 <minsung060607@gmail.com>
2022-07-06 20:23:52 +0900

    codeup1461 2rd commit

---
+++ 1461.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int num1[100][100]={},num2,num3=1;
+    scanf("%d",&num2);
+    for(int i=0;i<num2;i++){
+        for(int j=num2-1;j>=0;j--){
+            num1[i][j]=num3;
+            num3++;
+        }
+    }
+    for(int i=0;i<num2;i++){
+        for(int j=0;j<num2;j++){
+            printf("%d ",num1[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
